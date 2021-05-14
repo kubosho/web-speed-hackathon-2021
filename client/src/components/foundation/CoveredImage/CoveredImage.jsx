1c1fb02c7ab83269ec7b9cecd5bf735a9f8957d5
Shota Kubota <ta2@o2p.jp>
2021-05-14 19:50:24 +0900

    feat(CoveredImage): set width and height

--- client/src/components/foundation/CoveredImage/CoveredImage.jsx
+++ client/src/components/foundation/CoveredImage/CoveredImage.jsx
@@ -51,6 +51,8 @@ const CoveredImage = ({ alt, src }) => {
           'w-full h-auto': containerRatio <= imageRatio,
         })}
         src={blobUrl}
+        width={imageSize?.width}
+        height={imageSize?.height}
       />
     </div>
   );
